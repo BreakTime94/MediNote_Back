84068dc97d5413d6257a0a1c00683cd104cb5bb9
BreakTime94 <kimchan9408@gmail.com>
2025-09-09 13:11:36 +0900

    Back 레포 첫 커밋

---
+++ medinote_back_khs/src/main/java/com/medinote/medinote_back_khs/MedinoteBackKhsApplication.java
@@ -0,0 +1,13 @@
+package com.medinote.medinote_back_khs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedinoteBackKhsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedinoteBackKhsApplication.class, args);
+	}
+
+}
---
+++ medinote_back_kys/src/main/java/com/medinote/medinote_back_kys/MedinoteBackKysApplication.java
@@ -0,0 +1,13 @@
+package com.medinote.medinote_back_kys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedinoteBackKysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedinoteBackKysApplication.class, args);
+    }
+
+}
