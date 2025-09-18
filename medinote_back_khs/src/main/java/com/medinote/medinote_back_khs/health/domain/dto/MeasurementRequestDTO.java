5140a7d1001bb46b55fa889f3ecb6c54c629d13a
kimheeseon0526 <xuan6.6j@gmail.com>
2025-09-18 20:53:46 +0900

    -build.gradle 수정 -Measurement.java 미사용 import 삭제 -MeasurementRequestDTO.java - 필드 만드는 중

--- medinote_back_khs/src/main/java/com/medinote/medinote_back_khs/health/domain/dto/MeasurementRequestDTO.java
+++ medinote_back_khs/src/main/java/com/medinote/medinote_back_khs/health/domain/dto/MeasurementRequestDTO.java
@@ -1,4 +1,22 @@
 package com.medinote.medinote_back_khs.health.domain.dto;
 
+import com.medinote.medinote_back_khs.health.domain.en.MeasurementStatus;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MeasurementRequestDTO {
+
+    @NotNull(message = "회원ID(은)는 필수 입니다.")
+    private Long memberId;
+
+    @NotNull(message = "성별(은)는 필수 입니다.")
+    private MeasurementStatus gender;
+
 }
