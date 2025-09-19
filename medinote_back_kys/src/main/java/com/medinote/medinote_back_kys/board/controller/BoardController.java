4e9a995c5c91b065cf9807d227032a2236c0dd95
kys <tiger2ys98@gmail.com>
2025-09-19 18:07:17 +0900

    250919 restcontroller 입력 기능 구현

--- medinote_back_kys/src/main/java/com/medinote/medinote_back_kys/board/controller/BoardController.java
+++ medinote_back_kys/src/main/java/com/medinote/medinote_back_kys/board/controller/BoardController.java
@@ -1,4 +1,24 @@
 package com.medinote.medinote_back_kys.board.controller;
 
+import com.medinote.medinote_back_kys.board.domain.dto.BoardCreateRequestDTO;
+import com.medinote.medinote_back_kys.board.service.BoardService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/boards")
+@RequiredArgsConstructor
 public class BoardController {
+    private final BoardService boardService;
+
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<String> createBoard(@Valid @RequestBody BoardCreateRequestDTO dto) {
+        boardService.createBoard(dto);
+        return ResponseEntity.ok("OK");
+    }
 }
