46a3b3a592be8fbe9d6c9db6c815c7c8ca6fd05f
luminous77777 <tiger2ys98@gmail.com>
2025-09-22 04:35:20 +0900

    250922 보드 수정용 dto 생성

--- medinote_back_kys/src/main/java/com/medinote/medinote_back_kys/board/domain/dto/BoardUpdateRequestDTO.java
+++ medinote_back_kys/src/main/java/com/medinote/medinote_back_kys/board/domain/dto/BoardUpdateRequestDTO.java
@@ -22,7 +22,7 @@ public class BoardUpdateRequestDTO {
     @NotBlank(message = "내용은 비워둘 수 없습니다.")
     private String content;
 
-    private String isPublic;
+    private Boolean isPublic;
 
     private Boolean requireAdminPost;
 
