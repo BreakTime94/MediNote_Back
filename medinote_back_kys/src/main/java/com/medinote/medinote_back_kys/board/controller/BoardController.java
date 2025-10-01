451a2a9cfb2a45f46830277c99893950fa629551
kys <tiger2ys98@gmail.com>
2025-10-01 21:35:08 +0900

    cors 테스트용 설정

--- medinote_back_kys/src/main/java/com/medinote/medinote_back_kys/board/controller/BoardController.java
+++ medinote_back_kys/src/main/java/com/medinote/medinote_back_kys/board/controller/BoardController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/boards")
 @RequiredArgsConstructor
 @Validated
+@CrossOrigin(origins = "http://localhost:6006")
 public class BoardController {
     private final BoardService boardService;
 
